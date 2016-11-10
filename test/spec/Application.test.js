ef44638583647d6a8cad2b2274d34e9774fc1e5f
Kai Volland <kaivolland@yahoo.de>
2016-11-10 15:18:13 +0100

    Get rid of old testsuit (mocha, istanbul, phantom, sinon) stuff.

--- test/spec/Application.test.js
+++
@@ -1,7 +0,0 @@
-describe('MoMo.admin.Application', function() {
-    describe('Basics', function() {
-        it('is defined', function() {
-            expect(MoMo.admin.Application).to.not.be(undefined);
-        });
-    });
-});
